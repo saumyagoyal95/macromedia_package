e85889ab650f245a97d3b291f9fdd12f123884c6
Saumya Goyal <saumyagoyal95@gmail.com>
2021-10-21 15:46:02 +0200

    version 1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
  
 setuptools.setup(
    name = "macromedia_package",
-   version = "0.2",
+   version = "1.0",
    author = "Macromedia Prof",
    author_email = "author@example.com",
    description = "A small example package - for how to upload on Github",
